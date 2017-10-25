85792603e1c57ffb440494a433a659a4e9f5a8ba
Dominik Schloesser <dsc@dosc.net>
2017-10-25 16:41:30 +0200

    more tests

--- platform.h
+++ platform.h
@@ -1,20 +1,21 @@
 // platform.h - adapt platform specific stuff
 #pragma once
 
-#ifdef __linux__
-#include <stdio.h>
-#include <stdlib.h>
+#if defined(__linux__) || defined(__APPLE__)
 #include <cassert>
 #include <climits>
-#include <string>
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 #include <sys/time.h>
 #define USTD_ASSERTS 1
 typedef std::string String;
+#pragma message("Unixoid")
 #endif
 
 #ifndef UINT_MAX
-#define UINT_MAX (65535)  // or 4294967295 (mostly)
+#define UINT_MAX (65535) // or 4294967295 (mostly)
 #endif
 
 namespace ustd {
@@ -24,7 +25,7 @@ unsigned long timeDiffMicros(unsigned long first, unsigned long second) {
     return (unsigned long)-1 - first + second + 1;
 }
 
-#ifdef __linux__
+#if defined(__linux__) || defined(__APPLE__)
 unsigned long micros() {
     struct timeval tv;
     gettimeofday(&tv, 0);
@@ -32,4 +33,4 @@ unsigned long micros() {
 }
 #endif
 
-}  // namespace ustd
+} // namespace ustd
