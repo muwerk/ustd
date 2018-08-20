dc5eeb1800c2418bca0cb8f935316a517ad4b7e0
Dominik Schloesser <dsc@dosc.net>
2018-08-20 17:43:39 +0200

    Doc updated from master

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['add',['add',['../classustd_1_1array.html#afc3ff042affbb40184f8a0526b9e04f7',1,'ustd::array']]],
   ['alloclen',['alloclen',['../classustd_1_1array.html#a16dfdc7046e7749e9f0c93c365062bec',1,'ustd::array']]],
-  ['array',['array',['../classustd_1_1array.html',1,'ustd::array&lt; T &gt;'],['../classustd_1_1array.html#a034521812b6a4fc17f2a7dbb729a5616',1,'ustd::array::array()']]],
+  ['array',['array',['../classustd_1_1array.html',1,'ustd::array&lt; T &gt;'],['../classustd_1_1array.html#a73c2b783f194317522106e1ead813b52',1,'ustd::array::array()']]],
   ['array_2eh',['array.h',['../array_8h.html',1,'']]],
   ['array_3c_20k_20_3e',['array&lt; K &gt;',['../classustd_1_1array.html',1,'ustd']]],
   ['array_3c_20v_20_3e',['array&lt; V &gt;',['../classustd_1_1array.html',1,'ustd']]],
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['add',['add',['../classustd_1_1array.html#afc3ff042affbb40184f8a0526b9e04f7',1,'ustd::array']]],
   ['alloclen',['alloclen',['../classustd_1_1array.html#a16dfdc7046e7749e9f0c93c365062bec',1,'ustd::array']]],
-  ['array',['array',['../classustd_1_1array.html#a034521812b6a4fc17f2a7dbb729a5616',1,'ustd::array']]]
+  ['array',['array',['../classustd_1_1array.html#a73c2b783f194317522106e1ead813b52',1,'ustd::array']]]
 ];
