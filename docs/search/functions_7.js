868438be3ab0c442665cb19a37a9febacb20da36
Dominik Schloesser <dsc@dosc.net>
2018-08-27 17:18:55 +0200

    map doc

--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['peak',['peak',['../classustd_1_1map.html#ad3518e2b0ce40d5b27f7f62ffe6afea3',1,'ustd::map::peak()'],['../classustd_1_1queue.html#a184ff001f8f7c44f294b3f88147b5ad0',1,'ustd::queue::peak()']]],
-  ['pop',['pop',['../classustd_1_1queue.html#a0cb67072ac013f5c8b9b408d1fb17148',1,'ustd::queue']]],
-  ['push',['push',['../classustd_1_1queue.html#abd8f59460ac0d5fc80d99e105dfa1b58',1,'ustd::queue']]]
+  ['pop',['pop',['../classustd_1_1queue.html#a32d4d9b9d452c7c65150e66ab2e3f4e7',1,'ustd::queue']]],
+  ['push',['push',['../classustd_1_1queue.html#a8efb70bafecb8d8e5638ce5cbf54603f',1,'ustd::queue']]]
 ];
