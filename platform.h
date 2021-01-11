5929e03a939db2c7951957e438ec2312fd289130
Dominik Schlösser <dsc@dosc.net>
2021-01-11 19:43:51 +0100

    freeMemory function for Arduinos

--- platform.h
+++ platform.h
@@ -150,6 +150,27 @@ bool assertFailedLine(const char *filename, int line) {
 
 #endif  // end linux, apple
 
+#ifdef __ATMEGA__
+// from: https://learn.adafruit.com/memories-of-an-arduino/measuring-free-memory
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char *sbrk(int incr);
+#else   // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+int freeMemory() {
+    char top;
+#ifdef __arm__
+    return &top - reinterpret_cast<char *>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+    return &top - __brkval;
+#else   // __arm__
+    return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
+}
+#endif
+
 #ifdef USE_SERIAL_DBG
 
 #define DBG_INIT(f) Serial.begin(f)
